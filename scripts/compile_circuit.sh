c82f17bae63ceb7abe6761665e72b1e72a0fbae3
biscuitdey <biswashreedey@gmail.com>
2023-09-14 09:42:52 +0530

    Added Test circuit

--- scripts/compile_circuit.sh
+++ scripts/compile_circuit.sh
@@ -6,6 +6,6 @@ mkdir -p $BUILD_DIR &&
 circom ./baseline/circuits/$CIRCUIT_NAME.circom --r1cs --wasm --prime secq256k1 -o $BUILD_DIR &&
 
 # Compile circom r1cs into binary
-cargo run --release --bin gen_spartan_inst $BUILD_DIR/$CIRCUIT_NAME.r1cs $BUILD_DIR/$CIRCUIT_NAME.circuit $NUM_PUB_INPUTS &&
+cargo run --release --bin gen_spartan_inst $BUILD_DIR/$CIRCUIT_NAME.r1cs $BUILD_DIR/$CIRCUIT_NAME.circuit $NUM_PUB_INPUTS
 
 
